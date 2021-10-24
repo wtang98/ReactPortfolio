c0a1182c2a064040e00b090808f1cdeaba2aa39e
Wei-Tong Tang <weitongblue@gmail.com>
2021-10-24 14:27:35 +0100

    swaped page layout for home page

--- react-portfolio/src/pages/home/home.jsx
+++ react-portfolio/src/pages/home/home.jsx
@@ -8,14 +8,14 @@ const home = () => {
             <Header title ={"Home"}/>
             <div className="home__content d-flex justify-content-center flex-column align-items-center w-100">
                 <div className="home__content-landing">
-                    <h1 className="title">Hi! My Name is Wei-Tong Tang and i am a </h1>
+                    <h1 className="title m-0">Hi! My Name is Wei-Tong Tang and i am a:</h1>
                     <div className="text-wrapper d-flex justify-content-center">
                         <div className="text1">Software</div>
                         <div className="text2">React</div>
                         <div className="text3">JavaScript</div>
                     </div>
+                <h1 className="end">Developer</h1>
                 </div>
-                <h1>Developer</h1>
             </div>
         </div>
     )
